141f94c3b16612d3d7f656d4b9ecbc33fe43aaf8
Osuntuyi Amos <osuntuyiemilos@gmail.com>
2022-11-16 12:54:45 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ function App() {
             <p>{advice ? "“" + advice + "”" : "..."}</p>
           </div>
           <div className="divider"></div>
-          <button aria-label="Advice Refresh" onClick={handleJoke}>
+          <button aria-label="Refresh" onClick={handleJoke}>
             <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
               <path
                 d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z"
